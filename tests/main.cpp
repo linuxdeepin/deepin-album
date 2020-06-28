87b999d1835176ac557f2a7273312359f14a3148
zouya <zouya@uniontech.com>
2020-06-28 09:26:35 +0800

    feat:add unint test
    description:添加gtest单元测试框架

---
+++ tests/main.cpp
@@ -0,0 +1,10 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock-matchers.h>
+#include "application.h"
+
+int main(int argc, char *argv[])
+{
+    //Application a(argc, argv);
+    return RUN_ALL_TESTS();
+    //return a.exec();
+}
