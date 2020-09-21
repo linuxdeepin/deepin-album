14649fa75ee9a638e1802a0d9e976c310df4fbe0
zouya <zouya@uniontech.com>
2020-09-21 13:55:02 +0800

    fix: 解决wayland下设置壁纸失败问题
    wayland下使用专门逻辑获取屏幕名称
    
    Log: wayland下使用专门逻辑获取屏幕名称
    Change-Id: Idfc6edb3bca3479d81b403fbd832aeef8abb5798
    Reviewed-on: http://gerrit.uniontech.com/c/chengdu/deepin-album/+/5659
    Reviewed-by: <mailman@uniontech.com>
    Reviewed-by: zhanghao <zhanghao@uniontech.com>
    Reviewed-by: jixianglong <jixianglong@uniontech.com>
    Reviewed-by: dengjinhui <dengjinhui@uniontech.com>
    Reviewed-by: wangyu <wangyu@uniontech.com>
    Tested-by: <mailman@uniontech.com>

--- controller/wallpapersetter.cpp
+++ controller/wallpapersetter.cpp
@@ -29,64 +29,22 @@ WallpaperSetter::WallpaperSetter(QObject *parent) : QObject(parent)
 void WallpaperSetter::setWallpaper(const QString &path)
 {
     qDebug() << "SettingWallpaper 2";
-//    // gsettings unsupported unicode character
-//    QString tmpImg = QString("/tmp/DIVIMG.%1").arg(QFileInfo(path).suffix());
-//    QFile(path).copy(tmpImg);
-//    QImage img(tmpImg);
-//    if (img.format() != QImage::Format_Invalid) {
-//        if (img.save(QString("/tmp/DIVIMG.%1").arg("JPG"))) {
-//            //if convert image succeed,remove copy image.
-//            QFile(tmpImg).remove();
-//            //change value of tmpImg
-//            tmpImg = QString("/tmp/DIVIMG.%1").arg("JPG");
-//        }
-//    }
-
-//    if (!qEnvironmentVariableIsEmpty("FLATPAK_APPID")) {
     QDBusInterface interface("com.deepin.daemon.Appearance",
                                  "/com/deepin/daemon/Appearance",
                                  "com.deepin.daemon.Appearance");
     if (interface.isValid()) {
-        QString mm = dApp->primaryScreen()->name();
+        QString mm;
+        if (Application::isWaylandPlatform()) {
+            QDBusInterface interface("com.deepin.daemon.Display", "/com/deepin/daemon/Display", "com.deepin.daemon.Display");
+            mm = qvariant_cast< QString >(interface.property("Primary"));
+        } else {
+            mm = dApp->primaryScreen()->name();
+        }
         QDBusMessage reply = interface.call(QStringLiteral("SetMonitorBackground"), mm, path);
         qDebug() << "SettingWallpaper: replay" << reply.errorMessage();
     } else {
         qWarning() << "SettingWallpaper failed" << interface.lastError();
     }
-//    } else {
-//        DE de = getDE();
-//        //   qDebug() << "SettingWallpaper: " << de << path;
-//        switch (de) {
-//        case Deepin:
-//            setDeepinWallpaper(tmpImg);
-//            break;
-//        case KDE:
-//            setKDEWallpaper(tmpImg);
-//            break;
-//        case GNOME:
-//            setGNOMEWallpaper(tmpImg);
-//            break;
-//        case LXDE:
-//            setLXDEWallpaper(tmpImg);
-//            break;
-//        case Xfce:
-//            setXfaceWallpaper(tmpImg);
-//            break;
-//        default:
-//            setGNOMEShellWallpaper(tmpImg);
-//        }
-//    }
-
-
-
-//    // Remove the tmp file
-//    QTimer *t = new QTimer(this);
-//    t->setSingleShot(true);
-//    connect(t, &QTimer::timeout, this, [t, tmpImg] {
-//        QFile(tmpImg).remove();
-//        t->deleteLater();
-//    });
-//    t->start(1000);
 }
 
 void WallpaperSetter::setDeepinWallpaper(const QString &path)
