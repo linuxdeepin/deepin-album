302a37d1dba6e3a07c8587f475635c388d9225b4
guopengcheng <guopengcheng@uniontech.com>
2020-11-27 17:40:17 +0800

    fix: add timelineview case
    添加时间线相关的case
    
    Log: 提升产品稳定性
    Change-Id: I2d35ff27da953d57ca1d03886a3649653ce74f03

--- deepin-album/tests/timelineview/test_timelineview.cpp
+++ deepin-album/tests/timelineview/test_timelineview.cpp
@@ -6,13 +6,14 @@
 #include "../test_qtestDefine.h"
 
 #include "utils/baseutils.h"
+#include "imageengineapi.h"
+
 
 #include <QTestEventList>
 
 TEST(TimeLineView, T1)
 {
     qDebug() << "TimeLineView T1 count = " << count_testDefine++;
-//    QThreadPool::globalInstance()->waitForDone();
     MainWindow *w = dApp->getMainWindow();
     w->showEvent(nullptr);
     QTestEventList event;
@@ -29,3 +30,81 @@ TEST(TimeLineView, T1)
 
     ASSERT_TRUE(t->getIBaseHeight());
 }
+TEST(TimeLineView, dragPhotoToAnAlbum)
+{
+    qDebug() << "AlbumView dragPhotoToAnAlbum count = " << count_testDefine++;
+    MainWindow *w = dApp->getMainWindow();
+
+    w->timeLineBtnClicked();
+    QTest::qWait(100);
+    TimeLineView *a = w->m_pTimeLineView;
+    QList<QWidget *> widgets = a->findChildren<QWidget *>("");
+    for (int index = 0; index < widgets.count(); index++) {
+        if (!strcmp(widgets.at(index)->metaObject()->className(),("ThumbnailListView"))) {
+            QString jpgItemPath = testPath_test + "/2k9o1m.png";
+            QString text = "xxxxxxxxxxxxxx";
+            QIcon icon = QIcon(":/resources/images/other/deepin-album.svg");
+            QIcon icon_hover = QIcon(":/resources/images/other/deepin-album.svg");
+            QByteArray itemData;
+            QDataStream dataStream(&itemData, QIODevice::WriteOnly);
+            dataStream << text << icon << icon_hover;
+            QMimeData mimedata;
+            mimedata.setData(QStringLiteral("TestListView/text-icon-icon_hover"), itemData);
+            QList<QUrl> li;
+            li.append(QUrl::fromLocalFile(jpgItemPath));
+            mimedata.setUrls(li);
+            QTest::qWait(200);
+
+            const QPoint pos1 = a->rect().center();
+            QDragEnterEvent eEnter(pos1, Qt::IgnoreAction, &mimedata, Qt::LeftButton, Qt::NoModifier);
+            dApp->getDAppNew()->sendEvent(a, &eEnter);
+            QTest::qWait(200);
+
+            QDragMoveEvent eMove(pos1, Qt::IgnoreAction, &mimedata, Qt::LeftButton, Qt::NoModifier);
+            dApp->getDAppNew()->sendEvent(a, &eMove);
+            QTest::qWait(200);
+
+            QDropEvent e(pos1, Qt::IgnoreAction, &mimedata, Qt::LeftButton, Qt::NoModifier);
+            dApp->getDAppNew()->sendEvent(a, &e);
+            QTest::qWait(200);
+
+            dApp->getDAppNew()->sendEvent(a, &eEnter);
+            QTest::qWait(200);
+
+            QDragLeaveEvent eLeave;
+            dApp->getDAppNew()->sendEvent(a, &eLeave);
+            QTest::qWait(500);
+            break;
+        }
+    }
+}
+TEST(TimeLineView, SelectTimeLinesBtn)
+{
+    qDebug() << "TimeLineView SelectTimeLinesBtn count = " << count_testDefine++;
+    MainWindow *w = dApp->getMainWindow();
+    QTestEventList event;
+    w->timeLineBtnClicked();
+    QTest::qWait(500);
+    TimeLineView *t = w->m_pTimeLineView;
+    QList<QWidget *> widgets = t->findChildren<QWidget *>("");
+    foreach (auto widget, widgets) {
+        if (widget->objectName() == "TimeLineChooseButton") {
+            DCommandLinkButton *temp = static_cast<DCommandLinkButton*>(widget);
+            QPoint pos(10, 10);
+            event.addMouseMove(pos);
+            event.addMouseClick(Qt::MouseButton::LeftButton, Qt::NoModifier, pos);
+            event.simulate(temp);
+            event.clear();
+            QTest::qWait(500);
+
+            event.addMouseMove(pos);
+            event.addMouseClick(Qt::MouseButton::LeftButton, Qt::NoModifier, pos);
+            event.simulate(temp);
+            event.clear();
+            QTest::qWait(500);
+            break;
+        }
+    }
+}
+
+
