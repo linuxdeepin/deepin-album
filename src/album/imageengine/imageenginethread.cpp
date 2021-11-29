ea52a88fb82ae04e1bebc46d01e0ee04a57d9836
Zhang Yong <zhangyong@uniontech.com>
2021-11-29 14:45:45 +0800

    fix: 代码同步
    代码同步，解决外设导入中断时显示问题
    
    Log: 外设导入
    Change-Id: I9981a02eeb1ca0eadb88821b366b583d6857492a

--- src/album/imageengine/imageenginethread.cpp
+++ src/album/imageengine/imageenginethread.cpp
@@ -519,11 +519,12 @@ void ImageImportFilesFromMountThread::runDetail()
         QFileInfo fi(strNewPath);
         using namespace utils::image;
         using namespace utils::base;
+        //复制失败的图片不算在成功导入
         if (QFile::copy(strPath, strNewPath)) {
-//            qDebug() << "onCopyPhotoFromPhone()";
+            dbInfos << getDBInfo(strNewPath, utils::base::isVideo(strNewPath));
+        } else {
+            newPathList.removeOne(strNewPath);
         }
-        dbInfos << getDBInfo(strNewPath, utils::base::isVideo(strNewPath));
-
         emit dApp->signalM->progressOfWaitDialog(m_paths.size(), dbInfos.size());
     }
     ImageDataService::instance()->readThumbnailByPaths(newPathList, true);
@@ -605,7 +606,6 @@ void ImageLoadFromDBThread::runDetail()
 
     //删除数据库失效的图片
     DBManager::instance()->removeImgInfosNoSignal(fail_image_list);
-
     emit ImageEngineApi::instance()->sigReloadAfterFilterEnd();
 }
 
