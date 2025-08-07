22720d59268c97e8028e1ece16bc13af54de457c
Tian Shilin <tianshilin@uniontech.com>
2025-08-07 19:45:12 +0800

    feat: Update translations for Lao locale
    log: Translation update

---
+++ src/translations/desktop/desktop_lo.ts
@@ -0,0 +1,15 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS version="2.1" language="lo">
+<context>
+    <name>desktop</name>
+    <message>
+        <location filename="Desktop Entry]GenericName" line="0"/>
+        <source>Album</source>
+        <translation>אלבום</translation>
+    </message>
+    <message>
+        <location filename="Desktop Entry]Name" line="0"/>
+        <source>Deepin Album</source>
+        <translation>Deepin อัลบัม</translation>
+    </message>
+</context>
+</TS>
